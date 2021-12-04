32da3198a3a3c7350282daf83620b41380372ba4
Mateo Galic <mateo.galic@asynclabs.co>
2021-12-04 18:07:29 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">Hello</header>
+    </div>
+  )
+}
+
+export default App
